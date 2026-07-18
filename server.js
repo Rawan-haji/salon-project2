354bb3906ac8c4e4843df9f005f0c373f10e85c5
Rawan-haji <rawanjan02@gmail.com>
2026-07-19 01:04:05 +0300

    crete form in home page for sign-in

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const methodOverride = require('method-override')
 const path = require('path') 
 const app = express() 
 const PORT = 3700 
+const morgan = require("morgan")
+const session = require('express-session')
 
 
 // Mongoose connection 
@@ -38,3 +40,22 @@ app.use(session({
 }))
 app.use(passUserToView)
 
+//to home page
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
+
+// send sign-in when the user click to the button 
+app.post('/auth/sign-in',(req,res)=>{
+    const username=req.body.username
+    const password=req.body.password
+
+    console.log(`user trying to sign-in ,${username}`)
+    res.send(`thank you for sign-in ${username}`)
+})
+
+
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`)
+})
